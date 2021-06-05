1bda1c9b056e31729443eab79235602cd0ebce86
ColourBright <katerinka_post@mail.ru>
2021-06-05 22:19:06 +0500

    Rules, menu, level screens

--- static/index.js
+++ static/index.js
@@ -1,5 +1,5 @@
-// import * as path from "path";
-// import fs from "fs";
+//import * as path from "/path";
+//import fs from "/fs";
 
 const EasyLevelButton = document.getElementById('easyLevel');
 const MediumLevelButton = document.getElementById('mediumLevel');
