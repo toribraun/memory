835f24a60bab43c39ebd4e29070f3065f280a9ee
AngyBlacwood <saldina_2011@mail.ru>
2021-06-07 00:41:19 +0500

    cleared localStorage

--- static/leaderBoard.js
+++ static/leaderBoard.js
@@ -29,26 +29,4 @@ function getSortedScores() {
     return scores;
 }
 
-localStorage.setItem('Bob', '300')
-localStorage.setItem('ALice', '700')
-localStorage.setItem('Test', '300')
-localStorage.setItem('a', '300')
-localStorage.setItem('b', '700')
-localStorage.setItem('c', '300')
-localStorage.setItem('d', '300')
-localStorage.setItem('e', '700')
-localStorage.setItem('f', '300')
-localStorage.setItem('h', '300')
-localStorage.setItem('t', '700')
-localStorage.setItem('k', '300')
-localStorage.setItem('l', '300')
-localStorage.setItem('m', '700')
-localStorage.setItem('n', '300')
-localStorage.setItem('t', '700')
-localStorage.setItem('q', '300')
-localStorage.setItem('w', '300')
-localStorage.setItem('z', '700')
-localStorage.setItem('zzz', '300')
-localStorage.setItem('zz', '700')
-localStorage.setItem('zzzz', '300')
 renderLeaderboard();
