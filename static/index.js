23ea15b5fb456d429073ace953eff247d27a7041
ColourBright <katerinka_post@mail.ru>
2021-06-07 00:11:31 +0500

    changed cards to png

--- static/index.js
+++ static/index.js
@@ -5,9 +5,9 @@ const ShowRulesButton = document.getElementById('rulesButton');
 const CloseRulesButton = document.getElementById('backFromRules');
 const InputNameForm = document.getElementById('inputName');
 const defaultBG = 'url("images/back_card.png")'
-const IMAGES = ['01.png', '02.png', '03.png', '04.png', '05.png', '06.png',
-    '07.png', '08.png', '09.png', '10.png', '11.png', '12.png', '13.png',
-    '14.png', '15.png', '16.png', '17.png', '18.png', '19.png', '20.png'];
+const IMAGES = ['01.JPG', '02.JPG', '03.JPG', '04.JPG', '05.JPG', '06.JPG',
+    '07.JPG', '08.JPG', '09.JPG', '10.JPG', '11.JPG', '12.JPG', '13.JPG',
+    '14.JPG', '15.JPG', '16.JPG', '17.JPG', '18.JPG', '19.JPG', '20.JPG'];
 let STATE;
 let soundIsOn = checkSoundState();
 
